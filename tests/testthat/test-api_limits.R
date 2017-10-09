47c6cc4505b624f141d3fa3a5070cc6ce9a1cbc5
Andreas Beger <adbeger@gmail.com>
2017-10-09 21:39:18 +0300

    add function to check NYT API limits and usage (#21)
    * add function to check NYT API limits and usage
    
    - api_limits()
    - added tests for it also
    
    * fix header path for api_limits("geo")
    
    API requires at least one filter

---
+++ tests/testthat/test-api_limits.R
@@ -0,0 +1,17 @@
+context("api_limits")
+
+key <- Sys.getenv("NYTIMES_AS_KEY")
+
+test_that("returns list with limits", {
+  skip_on_cran()
+  
+  expect_is(api_limits("as"), "list")
+  expect_is(api_limits("geo"), "list")
+})
+
+test_that("fails well", {
+  skip_on_cran()
+  
+  expect_error(api_limits(key = ""), "need an API key for NYTIMES_AS_KEY")
+  expect_error(api_limits(api = "foo"), "only works for")
+})
